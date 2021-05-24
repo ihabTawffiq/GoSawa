f1446d9554266f63b86a212574ddad87bde25ae0
Tarekmohamed97 <tm7271420@gmail.com>
2021-05-24 14:07:26 +0200

    Modifying some logic and adding the sidebar

--- src/Components/navbar/navbar.js
+++ src/Components/navbar/navbar.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import MenuIcon from '@material-ui/icons/Menu';
 import { connect } from "react-redux";
 import "./navbar.css";
 import Logo from '../../goSawa.jpg'
@@ -24,60 +28,97 @@ const useStyles = makeStyles((theme) => ({
 
 function Nav(props) {
   const { auth, admin, owner, SignOut } = props;
+  const [navbarState, setNavbarState] = React.useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setNavbarState(open);
+  };
+
   return (
       <div className="navBar-container">
         <Link to="/" className="brand-logo">
           <img className = "logo-image-TM" src = {Logo}/>
           GOSAWA
         </Link>
-        <ul id="nav-mobile">
-          {owner ? (
-            <li>
-              <Link to="/add-admin" className="navBar-links-TM">Add Admin</Link>
-            </li>
-          ) : null}
+        {
+          auth.uid ? (
+            <div>
+              <div className = "openDrawer-button-TM" onClick={toggleDrawer(true)}>
+              <MenuIcon />
+              </div>
+              <Drawer anchor={'right'} open={navbarState} onClose={toggleDrawer(false)}>
+                <div className = "sideBar-container" onClick={toggleDrawer(false)}>
+                  <div  className = "brandLogo-sidebar-TM">
+                      <img src = {Logo}/>
+                      GOSAWA
+                  </div>
+                  <ul id="nav-mobile">
+                    {owner ? (
+                      <div>
+                        <li className="navBar-links-TM">
+                          <Link to="/add-admin"  style = {{color: "white"}}>Add Admin</Link>
+                        </li>
+                        <Divider />
+                      </div>
+                    ) : null}
 
-          {owner || admin ? (
-            <li>
-              <Link to="/add-route" className="navBar-links-TM">Add Route</Link>
-            </li>
+                    {owner || admin ? (
+                      <div className = "navBar-links-TM">
+                        <li>
+                          <Link to="/add-route" style = {{color: "white"}}>Add Route</Link>
+                        </li>
+                        <Divider />
+                      </div>
+                    ) : null}
 
-          ) : null}
+                    {owner || admin ? (
+                      <div className="navBar-links-TM">
+                        <li>
+                          <Link to="/add-captain" style = {{color: "white"}}>Add Captain</Link>
+                        </li>
+                        <Divider />
+                      </div>
+                    ) : null}
 
-          {owner || admin ? (
-            <li>
-              <Link to="/add-captain" className="navBar-links-TM">Add Captain</Link>
-            </li>
 
-          ) : null}
+                    {owner || admin ? (
+                      <div className="navBar-links-TM">
+                        <li>
+                          <Link to="/add-car" style = {{color: "white"}}>Add Car</Link>
+                        </li>
+                        <Divider />
+                      </div>
+                    ) : null}
 
-
-          {owner || admin ? (
-            <li>
-              <Link to="/add-car" className="navBar-links-TM">Add Car</Link>
-            </li>
-
-          ) : null}
-
-          {auth.uid ? (
-            <li>
-              <Link 
-                className="navBar-links"
-                to="/#"
-                onClick={(e) => {
-                  e.preventDefault();
-                  SignOut();
-                }}
-              >
-                Logout
-              </Link>
-            </li>
+                    {auth.uid ? (
+                      <div className="navBar-links-TM">
+                        <li>
+                          <Link 
+                            style = {{color: "white"}}
+                            to="/#"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              SignOut();
+                            }}
+                          >
+                            Logout
+                          </Link>
+                        </li>
+                        <Divider />
+                      </div>
+                    ) : null}
+                  </ul>
+                </div>
+              </Drawer>
+            </div>
           ) : (
-            <li>
-              <Link to="/login" className="navBar-links-TM">Login</Link>
-            </li>
-          )}
-        </ul>
+              <Link to="/login" style = {{color: "white", marginTop: 20}}>Login</Link>
+          )
+        }
       </div>
   );
 }
